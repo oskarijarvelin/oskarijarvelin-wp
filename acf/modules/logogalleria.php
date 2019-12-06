2823b9210e6d7d7fc6a0995897137c34660a9f61
Oskari Järvelin <contact@oskarijarvelin.fi>
2019-12-06 23:25:59 +0200

    Fixed links

--- acf/modules/logogalleria.php
+++ acf/modules/logogalleria.php
@@ -10,8 +10,8 @@
 	<div class="row">
 		<?php foreach ( get_sub_field('logot') as $logo ) : ?>
 			<div class="col-12 col-sm-6 col-lg-4 col-xl-3 ">
-				<a href="#" class="d-flex w-100 h-100 justify-content-center align-items-center py-5 px-5 px-lg-4">
-					<img src="<?php echo $logo['sizes']['large']; ?>" width="100%" height="auto">
+				<a href="<?php echo get_field('linkki', $logo['ID']); ?>" target="_blank" rel="noopener" class="d-flex w-100 h-100 justify-content-center align-items-center py-5 px-5 px-lg-4">
+					<img src="<?php echo $logo['sizes']['large']; ?>" width="100%" height="auto" alt="Logo - <?php echo $logo['alt']; ?>">
 				</a>
 			</div>
 		<?php endforeach; ?>
